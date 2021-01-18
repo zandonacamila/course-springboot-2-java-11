08c6143dfe3a7ba308172faf459a7f0415baf0f3
Camila Zandoná <zandona.camila@gmail.com>
2021-01-18 14:05:33 -0300

    Revert "Order, Instant, ISO 8601 - PART 1"
    This reverts commit 10aefefe4ca49985112ee52c26af342456651060.

--- src/main/java/com/example/couse/entities/User.java
+++ src/main/java/com/example/couse/entities/User.java
@@ -1,18 +1,13 @@
 package com.example.couse.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -24,9 +19,6 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
-	
 	public User() {}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -36,11 +28,6 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
-	}
-	
-
-	public List<Order> getOrders() {
-		return orders;
 	}
 
 	public Long getId() {
